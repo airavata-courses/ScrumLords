c9157ec8df7bf751a96d9dcb6b554e5dc359caa0
bobbyrathoree <bobbyprathore@gmail.com>
2020-01-30 20:28:06 -0500

    [ADD] data retrieval skeleton.

---
+++ weather_data_retrieval/data_retrieval/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DataRetrievalConfig(AppConfig):
+    name = 'data_retrieval'
---
+++ weather_data_retrieval/weather_data_retrieval/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for weather_data_retrieval project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weather_data_retrieval.settings')
+
+application = get_wsgi_application()
