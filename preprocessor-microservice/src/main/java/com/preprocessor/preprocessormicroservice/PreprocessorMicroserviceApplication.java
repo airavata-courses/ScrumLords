40eea396be924c7706f644ddf40f489449d01717
Yashvardhan Jain <yashvrdnjain@gmail.com>
2020-02-02 15:50:03 -0500

    [ADD] Java Spring Boot Skeleton

---
+++ preprocessor-microservice/src/main/java/com/preprocessor/preprocessormicroservice/PreprocessorMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.preprocessor.preprocessormicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreprocessorMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreprocessorMicroserviceApplication.class, args);
+	}
+
+}
---
+++ preprocessor-microservice/src/test/java/com/preprocessor/preprocessormicroservice/PreprocessorMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.preprocessor.preprocessormicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreprocessorMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
