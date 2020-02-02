933d822ca50a4ef521a6e06c62fb77c97f039594
J-Yash <yashvrdnjain@gmail.com>
2020-02-02 17:24:20 -0500

    Added web dependencies and a REST controller

--- preprocessor-microservice/src/main/java/com/preprocessor/preprocessormicroservice/PreprocessorMicroserviceApplication.java
+++ preprocessor-microservice/src/main/java/com/preprocessor/preprocessormicroservice/PreprocessorMicroserviceApplication.java
@@ -2,12 +2,31 @@ package com.preprocessor.preprocessormicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
 
 @SpringBootApplication
 public class PreprocessorMicroserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PreprocessorMicroserviceApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Let's inspect the beans:");
+			
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.parallelSort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+		};
 	}
 
 }
