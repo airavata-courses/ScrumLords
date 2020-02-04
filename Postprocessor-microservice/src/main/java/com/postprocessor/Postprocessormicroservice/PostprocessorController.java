793fcfe4df91bef5baa1e2f6fe5dd355a8647ebd
J-Yash <yashvrdnjain@gmail.com>
2020-02-04 14:57:42 -0500

    Minor api change

--- Postprocessor-microservice/src/main/java/com/postprocessor/Postprocessormicroservice/PostprocessorController.java
+++ Postprocessor-microservice/src/main/java/com/postprocessor/Postprocessormicroservice/PostprocessorController.java
@@ -1,14 +1,21 @@
 package com.postprocessor.Postprocessormicroservice;
 
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PostprocessorController {
 	
-	@RequestMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+	
+	@GetMapping("/getcontent")
+	public Postprocessor postprocess(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Postprocessor(counter.incrementAndGet(), String.format(template, name));
 	}
 
 }
