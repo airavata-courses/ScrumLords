d373da9b3ff592f281a9b0b1dfde7578a153235d
bobbyrathoree <bobbyprathore@gmail.com>
2020-02-06 16:03:12 -0500

    [ADD] get all sessions for user.

---
+++ manager/api_manager/pubsub/views/session_get.py
@@ -0,0 +1,15 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from api_manager.models import Session
+from api_manager.serializers import SessionSerializer
+
+
+@api_view(["GET"])
+def get_all_sessions(request, user_id):
+    response = SessionSerializer(
+        Session.objects.filter(user_id=user_id).all(),
+        many=True,
+    ).data
+    return Response({"data": response}, status=status.HTTP_200_OK)
--- manager/manager/urls.py
+++ manager/manager/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path
 
 from api_manager.pubsub.views.session_create import start_session_pipeline
+from api_manager.pubsub.views.session_get import get_all_sessions
 from api_manager.pubsub.views.session_status import get_session_status, update_session_status
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path(r"session/create", start_session_pipeline),
     url(r"^session/(?P<session_id>\w+)/status", get_session_status),
-    url(r"^session/status", update_session_status)
+    url(r"^session/status", update_session_status),
+    url(r"^user/(?P<user_id>\w+)/sessions", get_all_sessions),
 ]
