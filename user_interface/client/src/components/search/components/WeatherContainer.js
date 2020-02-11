1659e15950780a06d4f5217cb51e8f8a38a7f915
Dhruv Yadwadkar <dhruvyadwadkar@gmail.com>
2020-02-10 21:47:00 -0500

    Added City ID

--- user_interface/client/src/components/search/components/WeatherContainer.js
+++ user_interface/client/src/components/search/components/WeatherContainer.js
@@ -4,47 +4,63 @@ import axios from 'axios'
 
 export const WeatherContainer = () => {
     const API_KEY = '4c3f87318d7dda9dbf7b495c8c670333'
-    const [searchQuery, setSearchQuery] = useState();
+    const [searchQuery1, setSearchQuery1] = useState();
+    const [searchQuery2, setSearchQuery2] = useState();
     const [weatherData, setWeatherData] = useState({
         temp: null,
         humidity: null,
         desc: null,
         city: null
     })
-    const [isValidZip, setIsValidZip] = useState(true);
 
+    const updateSearch1 = e => {
+        setSearchQuery1(e.target.value);
+    }
+
+    const updateSearch2 = e => {
+        setSearchQuery2(e.target.value);
+    }
+
+
+    /*
+    const [isValidZip, setIsValidZip] = useState(true);
+    
+    
     const updateSearch = e => {
         let zipCode = e.target.value;
         let isValid = validateZip(zipCode);
         setSearchQuery(e.target.value);
-
+    
         if (isValid || zipCode === '' || isValid.length === 5) {
             setIsValidZip(true);
         } else {
             setIsValidZip(false);
         }
-
+    
     }
-
+    
+    
     const validateZip = (zipCode) => {
         let regex = /[0-9]{5}/;
         return regex.test(zipCode)
     }
+    */
 
     const getWeatherData = () => {
+
+        /*
         if (!isValidZip || searchQuery === '') {
             setIsValidZip(false);
             return;
         }
+        */
 
-        fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${searchQuery},us&appid=${API_KEY}`)
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${searchQuery1},${searchQuery2},us&appid=${API_KEY}`)
             .then(res => res.json())
             .then(async data => {
-                const latitude = data.coord.lat;
-                const longitude = data.coord.lon;
                 const user = await axios.get('/api/auth');
                 const user_id = user.data._id
-
+                let city_id = data.id;
                 setWeatherData({
                     temp: data.main.temp,
                     humidity: data.main.humidity,
@@ -58,23 +74,47 @@ export const WeatherContainer = () => {
                         'Content-Type': 'application/json'
                     }
                 };
-                const body = JSON.stringify({ data: { user_id, latitude, longitude } });
+                const body = JSON.stringify({ data: { user_id, city_id } });
                 // for sending response 
                 const res = axios.post('http://localhost:8000/session/create', body, config);
                 console.log(body)
             })
     }
 
+    /*
     return (
         <section className="weather-container">
             <header className="weather-header">
                 <h3>WEATHER.IO</h3>
+                
                 <div>
                     <input placeholder="Enter Zip Code..." className="search-input" onChange={updateSearch} maxLength='5' />
                     <button className="material-icons" onClick={getWeatherData}>search</button>
                 </div>
+                
             </header>
             <p className="error">{isValidZip ? '' : 'Invalid Zip Code!'}</p>
+            <section className="weather-info">
+                {weatherData.temp === null ? (
+                    <p>No Weather to Display<i className="material-icons">wb_sunny</i></p>
+                ) : <WeatherInfo data={weatherData} />
+                }
+            </section>
+        </section>
+    )
+    */
+    return (
+        <section className="weather-container">
+            <header className="weather-header">
+                <h3>WEATHER.IO</h3>
+
+                <div>
+                    <input placeholder="City Name..." className="search-input1" onChange={updateSearch1} />
+                    <input placeholder="State" className="search-input2" onChange={updateSearch2} />
+                    <button className="material-icons" onClick={getWeatherData}>search</button>
+                </div>
+
+            </header>
             <section className="weather-info">
                 {weatherData.temp === null ? (
                     <p>No Weather to Display<i className="material-icons">wb_sunny</i></p>
