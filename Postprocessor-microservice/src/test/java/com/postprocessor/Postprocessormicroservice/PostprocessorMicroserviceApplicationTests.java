e59887c7c4aba9ca5ebf20637afa01264cc9e66c
bobbyrathoree <bobbyprathore@gmail.com>
2020-02-13 02:02:52 -0500

    [MODIFY] test cases file in post_process service.

--- Postprocessor-microservice/src/test/java/com/postprocessor/Postprocessormicroservice/PostprocessorMicroserviceApplicationTests.java
+++ Postprocessor-microservice/src/test/java/com/postprocessor/Postprocessormicroservice/PostprocessorMicroserviceApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PostprocessorMicroserviceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
