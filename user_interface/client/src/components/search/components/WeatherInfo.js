453e98f3bd4fa64b63d7ca88d3749beb729105ff
Dhruv Yadwadkar <dhruvyadwadkar@gmail.com>
2020-02-13 23:39:08 -0500

    [ADD] display session forecast

--- user_interface/client/src/components/search/components/WeatherInfo.js
+++ user_interface/client/src/components/search/components/WeatherInfo.js
@@ -6,16 +6,12 @@ const WeatherInfo = (props) => {
         <Fragment>
             <h3>{desc}</h3>
             <div className="header-description">
-                <h4>City</h4>
+
                 <p>{city}</p>
             </div>
             <div className="header-description">
-                <h4>Temperature</h4>
-                <p>{temp}</p>
-            </div>
-            <div className="header-description">
-                <h4>Humidity</h4>
-                <p>{humidity}</p>
+
+                <p>{temp}<sup>o</sup>F</p>
             </div>
         </Fragment>
     )
