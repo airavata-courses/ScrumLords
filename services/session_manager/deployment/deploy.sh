1cb5ec02629b64a63d513c3935f921bde2927e40
bobbyrathoree <bobbyprathore@gmail.com>
2020-03-13 03:33:02 -0400

    [ADD] horizonal pod scalers for all session_manager and model_execution.

--- services/session_manager/deployment/deploy.sh
+++ services/session_manager/deployment/deploy.sh
@@ -8,14 +8,14 @@ apt-get update && apt-get install -y jq
 echo -e "Done.\n\n"
 
 echo "Updating with latest commit tag..."
-jq --arg LC "$LC" '. + { "manager_version_tag": $LC }' deployment/production_jinja.json > deployment/updated_production_jinja.json
+jq --arg LC "$LC" '. + { "session_manager_version_tag": $LC }' deployment/production_jinja.json > deployment/updated_production_jinja.json
 echo -e "Done. Here's what the properties look like:\n"
 cat ./deployment/updated_production_jinja.json
 echo -e "\n\n"
 
 echo "Creating services, deployments and horizontal pod autoscalers from jinja templates..."
-j2 ./deployment/manager-service.yaml.jinja deployment/updated_production_jinja.json --format=json | kubectl apply -f -
-j2 ./deployment/manager-deployment.yaml.jinja deployment/updated_production_jinja.json --format=json | kubectl apply -f -
-j2 ./deployment/manager-hpa.yaml.jinja deployment/updated_production_jinja.json --format=json | kubectl apply -f -
+j2 ./deployment/session-manager-service.yaml.jinja deployment/updated_production_jinja.json --format=json | kubectl apply -f -
+j2 ./deployment/session-manager-deployment.yaml.jinja deployment/updated_production_jinja.json --format=json | kubectl apply -f -
+j2 ./deployment/session-manager-hpa.yaml.jinja deployment/updated_production_jinja.json --format=json | kubectl apply -f -
 
 echo -e "\n\nBuild complete."
