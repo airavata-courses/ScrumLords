d1c88c17ad3b04a20d8d1e4a1d8a54e986d7a97c
bobbyrathoree <bobbyprathore@gmail.com>
2020-03-19 17:08:33 -0400

    [ADD] cloudbuild yaml for weather data retrieval.

--- services/manager/api_manager/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from api_manager.models.city import City
-from api_manager.models.session import Session
-
-admin.site.register(Session)
-admin.site.register(City)
