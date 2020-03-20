d8f8e7bfc445b032ec6fd2a6a78242a247f7fd69
bobbyrathoree <bobbyprathore@gmail.com>
2020-03-20 16:03:01 -0400

    [ADD] test cases for build.

--- services/model_execution/ml_model/tests.py
+++ services/model_execution/ml_model/tests.py
@@ -6,6 +6,15 @@ from ml_model.views.forecast import forecast_weather
 
 class ModelExecutionApiTest(SimpleTestCase):
     def test_get_forecast_weather(self):
-        content = {'subscription': 'projects/falana-dhimka/subscriptions/model_execute_sub', 'message': {'data': 'eyJuX2RheXNfYWZ0ZXIiOiA3LCAibGF0aXR1ZGUiOiAiMzkuMTY1MzMiLCAibG9uZ2l0dWRlIjogIi04Ni41MjYzOSIsICJzZXNzaW9uX2lkIjogMzE1N30=', 'messageId': '3', 'attributes': {}}}
-        response = self.client.post('/dark/forecast', content, content_type="application/json")
+        content = {
+            "subscription": "projects/falana-dhimka/subscriptions/model_execute_sub",
+            "message": {
+                "data": "eyJuX2RheXNfYWZ0ZXIiOiA3LCAibGF0aXR1ZGUiOiAiMzkuMTY1MzMiLCAibG9uZ2l0dWRlIjogIi04Ni41MjYzOSIsICJzZXNzaW9uX2lkIjogMzE1N30=",
+                "messageId": "3",
+                "attributes": {},
+            },
+        }
+        response = self.client.post(
+            "/dark/forecast", content, content_type="application/json"
+        )
         self.assertEqual(response.status_code, 200)
