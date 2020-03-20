79896414c447761d53b1d58c1c8e3fc445ba788a
J-Yash <yashvrdnjain@gmail.com>
2020-03-20 16:06:20 -0400

    Added tests

--- services/manager/api_manager/tests.py
+++ services/manager/api_manager/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import SimpleTestCase, Client
 
 # Create your tests here.
+
+class ManagerApiTest(SimpleTestCase):
+    def test_health_check(self):
+        response = self.client.get("/ht")
+        self.assertEqual(response.status_code, 200)
+
+    def test_ready_check(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
