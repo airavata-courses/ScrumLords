cff0d951a9f54afea20979fb144d3cff5ffb629e
bobbyrathoree <bobbyprathore@gmail.com>
2020-03-20 16:05:01 -0400

    [ADD] test cases for build.

--- services/weather_data_retrieval/data_retrieval/tests.py
+++ services/weather_data_retrieval/data_retrieval/tests.py
@@ -6,6 +6,15 @@ from data_retrieval.views.data_retrieve import retrieve_historical_data
 
 class DataRetrievalApiTest(SimpleTestCase):
     def test_get_forecast_weather(self):
-        content = {'subscription': 'projects/falana-dhimka/subscriptions/data_retrieval_sub', 'message': {'data': 'eyJzZXNzaW9uX2lkIjogMzE1NywgIm5fZGF5c19iZWZvcmUiOiA1LCAibl9kYXlzX2FmdGVyIjogNywgImxhdGl0dWRlIjogIjM5LjE2NTMzIiwgImxvbmdpdHVkZSI6ICItODYuNTI2MzkifQ==', 'messageId': '1', 'attributes': {}}}
-        response = self.client.post('/dark/history', content, content_type="application/json")
+        content = {
+            "subscription": "projects/falana-dhimka/subscriptions/data_retrieval_sub",
+            "message": {
+                "data": "eyJzZXNzaW9uX2lkIjogMzE1NywgIm5fZGF5c19iZWZvcmUiOiA1LCAibl9kYXlzX2FmdGVyIjogNywgImxhdGl0dWRlIjogIjM5LjE2NTMzIiwgImxvbmdpdHVkZSI6ICItODYuNTI2MzkifQ==",
+                "messageId": "1",
+                "attributes": {},
+            },
+        }
+        response = self.client.post(
+            "/dark/history", content, content_type="application/json"
+        )
         self.assertEqual(response.status_code, 200)
