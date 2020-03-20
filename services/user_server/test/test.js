e96f38c5634c40f05d5a32882e700975fe1cd402
Dhruv Yadwadkar <dhruvyadwadkar@gmail.com>
2020-03-20 00:58:32 -0400

    [ADD] Register Test Case

--- services/user_server/test/test.js
+++ services/user_server/test/test.js
@@ -29,3 +29,19 @@ describe('Login API', function () {
 // Returns a JWT Token ---- > res.json({ token })
 // Error status 400
 
+describe('Register API', function () {
+    it('Valid Register!', function (done) {
+        request(app)
+            .post('/api/users')
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .send({ name: 'TEST', email: 'TEST1@gmail.com', password: '123456' })
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .expect(function (response) {
+                expect(response.body).not.to.be.empty;
+                expect(response.body).to.be.an('object');
+            })
+            .end(done);
+    });
+});
