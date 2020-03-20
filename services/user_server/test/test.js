eb2bad83225ddf8aa6bbbb6c90d129c9762767af
Dhruv Yadwadkar <dhruvyadwadkar@gmail.com>
2020-03-20 13:54:23 -0400

    [ADD] Final Test

--- services/user_server/test/test.js
+++ services/user_server/test/test.js
@@ -1,19 +1,21 @@
 const request = require('supertest');
 const expect = require('chai').expect;
 const app = require('../server');
+const User = require('../models/User');
 
-// Test route POST api/users
-// Registration Route for user
+// Test route POST api/auth
+// Login Route for user
 // Returns a JWT Token ---- > res.json({ token })
 // Error status 400
 
-describe('Login API', function () {
-    it('Valid Credentials!', function (done) {
+/*
+describe('Register API', function () {
+    it('Valid Register!', function (done) {
         request(app)
-            .post('/api/auth')
+            .post('/api/users')
             .set('Accept', 'application/json')
             .set('Content-Type', 'application/json')
-            .send({ email: 'testuser1@gmail.com', password: '123456' })
+            .send({ name: 'TEST', email: 'TEST@gmail.com', password: '123456' })
             .expect(200)
             .expect('Content-Type', /json/)
             .expect(function (response) {
@@ -23,19 +25,20 @@ describe('Login API', function () {
             .end(done);
     });
 });
+*/
 
-// Test route POST api/auth
-// Login Route for user
+// Test route POST api/users
+// Registration Route for user
 // Returns a JWT Token ---- > res.json({ token })
 // Error status 400
 
-describe('Register API', function () {
-    it('Valid Register!', function (done) {
+describe('User Authentication', function () {
+    it('Valid Credentials!', function (done) {
         request(app)
-            .post('/api/users')
+            .post('/api/auth')
             .set('Accept', 'application/json')
             .set('Content-Type', 'application/json')
-            .send({ name: 'TEST', email: 'TEST1@gmail.com', password: '123456' })
+            .send({ email: 'TEST@gmail.com', password: '123456' })
             .expect(200)
             .expect('Content-Type', /json/)
             .expect(function (response) {
